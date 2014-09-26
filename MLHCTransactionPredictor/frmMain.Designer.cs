4e67c35fc890e13765e9ecfefcbef5bd9a4999e5
Ben White <bwhite@mlhc.com>
2014-09-25 22:08:48 -0700

    Added Encog Core, ran through tutorial

--- MLHCTransactionPredictor/frmMain.Designer.cs
+++ MLHCTransactionPredictor/frmMain.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(12, 12);
+            this.txtMain.Multiline = true;
+            this.txtMain.Name = "txtMain";
+            this.txtMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMain.Size = new System.Drawing.Size(798, 217);
+            this.txtMain.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(13, 236);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(270, 50);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 485);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(822, 321);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtMain);
+            this.Name = "frmMain";
+            this.Text = "MLHC Transaction Predictor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.Button btnTest;
     }
 }
 
--- MLHCTransactionPredictor/frmMain.cs
+++ MLHCTransactionPredictor/frmMain.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Encog.Neural.Data.Basic;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Training;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
+using Encog.Neural.NeuralData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +21,43 @@ namespace MLHCTransactionPredictor
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            BasicNetwork network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(2));
+            network.AddLayer(new BasicLayer(2));
+            network.AddLayer(new BasicLayer(1));
+            network.Structure.FinalizeStructure();
+            network.Reset();
+
+            double[][] XOR_INPUT = {
+                                       new double[2] {0.0, 0.0},
+                                       new double[2] {1.0, 0.0},
+                                       new double[2] {0.0, 1.0},
+                                       new double[2] {1.0, 1.0}
+                                   };
+
+            double[][] XOR_IDEAL = {
+                                       new double[1] {0.0},
+                                       new double[1] {1.0},
+                                       new double[1] {1.0},
+                                       new double[1] {0.0}
+                                   };
+
+            INeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
+
+            ITrain train = new ResilientPropagation(network, trainingSet);
+
+            int epoch = 1;
+
+            do
+            {
+                train.Iteration();
+                txtMain.AppendText(String.Format("Epoch #{0}, Error:{1}{2}", epoch, train.Error, Environment.NewLine));
+                epoch++;
+            } while ((epoch < 5000) && (train.Error > 0.01));
         }
     }
 }
